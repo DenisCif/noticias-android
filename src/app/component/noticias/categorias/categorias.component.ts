98a89bd6a7bb5dbde55248d0fed04a1c4d3ad6ab
Denis <denisjoelcif@gmail.com>
2024-03-15 19:49:59 -0600

    implementaicon de logica

--- src/app/component/noticias/categorias/categorias.component.ts
+++ src/app/component/noticias/categorias/categorias.component.ts
@@ -1,14 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { categoriaDTO } from 'src/app/dto/Categoria.dto';
+import { CategoriaService } from 'src/app/service/categoria/categoria.service';
 
 @Component({
   selector: 'app-categorias',
   templateUrl: './categorias.component.html',
   styleUrls: ['./categorias.component.scss'],
 })
-export class CategoriasComponent  implements OnInit {
+export class CategoriasComponent implements OnInit {
 
-  constructor() { }
+  categorias: categoriaDTO[] | undefined;
 
-  ngOnInit() {}
+  constructor(private categoriaService: CategoriaService) { }
 
+  ngOnInit(): void {
+    this.obtenerCategorias();
+  }
+
+  obtenerCategorias(): void {
+    this.categoriaService.listarCategorias()
+      .subscribe(categorias => {
+        this.categorias = categorias;
+      });
+  }
+
+  guardarIdCategoria(idCategoria: number): void {
+    sessionStorage.setItem("idCategoria", idCategoria.toString());
+    window.location.reload();
+  }
+
+  quitarFiltro() {
+    sessionStorage.removeItem("idCategoria")
+    window.location.reload();
+  }
 }
+

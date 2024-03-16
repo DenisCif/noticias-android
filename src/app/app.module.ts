064c902c464dd6fb9776fe042e1ba96c094b27df
Denis <denisjoelcif@gmail.com>
2024-03-15 19:52:10 -0600

    logica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,11 +14,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './component/auth/login/login.component';
 import { InicioComponent } from './component/inicio/inicio/inicio.component';
 import { RegisterComponent } from './component/auth/register/register.component';
+import { DetalleNoticiaComponent } from './component/noticias/detalle-noticia/detalle-noticia.component';
+import { CategoriasComponent } from './component/noticias/categorias/categorias.component';
+import { NoticiasRelacionadasComponent } from './component/noticias/noticias-relacionadas/noticias-relacionadas.component';
+import { InicioNoticiasComponent } from './component/noticias/inicio-noticias/inicio-noticias.component';
 
 
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, InicioComponent, RegisterComponent],
+  declarations: [AppComponent, LoginComponent, InicioComponent, RegisterComponent, DetalleNoticiaComponent, NoticiasRelacionadasComponent,
+    CategoriasComponent, InicioNoticiasComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule, ReactiveFormsModule, HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
--- src/app/component/noticias/detalle-noticia/detalle-noticia.component.ts
+++ src/app/component/noticias/detalle-noticia/detalle-noticia.component.ts
@@ -1,14 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NoticiaDTO } from 'src/app/dto/Noticia.dto';
+import { NoticiaService } from 'src/app/service/noticia/noticia.service';
 
 @Component({
   selector: 'app-detalle-noticia',
   templateUrl: './detalle-noticia.component.html',
   styleUrls: ['./detalle-noticia.component.scss'],
 })
-export class DetalleNoticiaComponent  implements OnInit {
+export class DetalleNoticiaComponent implements OnInit {
 
-  constructor() { }
+  idNoticia: number = 0;
+  noticia: NoticiaDTO | undefined;
+  noticias: NoticiaDTO[] | undefined;
 
-  ngOnInit() {}
+
+  constructor(private route: ActivatedRoute, private noticiaService: NoticiaService) { }
+
+  ngOnInit(): void {
+    this.obtenerIDNoticia();
+    this.cargarNoticia();
+  }
+
+  cargarNoticia(): void {
+    this.noticiaService.verNoticia(this.idNoticia)
+      .subscribe(noticia => {
+        this.noticia = noticia;
+      });
+  }
+  obtenerIDNoticia() {
+    const idNoticiaString = sessionStorage.getItem("idNoticia");
+    if (idNoticiaString) {
+      this.idNoticia = parseInt(idNoticiaString, 10);
+    }
+  }
+
 
 }
